bc797560e4d2e678943308f06954fc43f4519181
gwt805 <1973735972@qq.com>
2025-01-06 11:59:57 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
+import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import disableDevtool from'disable-devtool';
 import 'element-plus/theme-chalk/dark/css-vars.css'
+
+disableDevtool({url: "https://www.baidu.com/",  timeOutUrl: "https://www.baidu.com/"});
 
 const app = createApp(App)
 app.use(ElementPlus)
