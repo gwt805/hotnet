9a76608779c9e1dfab5acb0dd317a00aef4e6f77
gwt805 <1973735972@qq.com>
2026-04-16 17:29:44 +0800

    更新接口

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
     <div class="common-layout">
-        <div class="div-f">
-            <el-card :key="idx" style="max-width: 480px" v-for="(v, idx) in card_data">
-                <div class="div-head">
-                    <h3>{{ v.name }}</h3>
-                </div>
-                <div class="inner">
-                    <ul class="hot-list-article">
-                        <li v-for="subItem in v.data" :key="subItem.index"> <a :href="subItem.url" target="_blank" draggable="false">{{subItem.title }}</a></li>
-                    </ul>
-                </div>
-            </el-card>
-        </div>
+        <el-card :key="idx" v-for="(v, idx) in hotlist_data">
+            <div class="div-head">
+                <h3>{{ v.name }}</h3>
+            </div>
+            <div class="inner">
+                <ul class="hot-list-article">
+                    <li v-for="subItem in v.value" :key="subItem.id"> <a :href="subItem.url" target="_blank" draggable="false" rel="noopener noreferrer">{{subItem.title }}</a></li>
+                </ul>
+            </div>
+        </el-card>
     </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
-import { ElLoading } from "element-plus";
 import { ref, onMounted, onUnmounted } from 'vue';
+import { ElLoading, ElMessage } from "element-plus";
 
-const card_data: any = ref([])
+const hotlist = ref([])
+const hotlist_data:any = ref([])
 const http = axios.create()
 onMounted(() => {
-    const loadingService = ElLoading.service({fullscreen: true, text: "正在加载资源 ~"});
-    http.get("https://api.vvhan.com/api/hotlist/all").then((res: any) => {
-        card_data.value = res.data.data;
+    let loadingService = ElLoading.service({fullscreen: true, text: "正在加载资源 ~"});
+    http.get("https://api.pearktrue.cn/api/dailyhot/").then((res:any)=>{
+        hotlist.value = res.data.data.platforms;
+        for (let i = 0; i < hotlist.value.length; i++) {
+            http.get(`https://api.pearktrue.cn/api/dailyhot/?title=${hotlist.value[i]}`).then((res: any) => {
+                if (res.data.data.length > 1) hotlist_data.value.push({"name": hotlist.value[i], "value": res.data.data.slice(0, 100)});
+            })
+        }
         loadingService.close();
-    }).catch(() => {
+    }).catch(()=>{
         loadingService.close();
+        ElMessage({ type: "error", message: "加载失败，请稍后再试 ~", duration: 1000 });
     })
     document.getElementsByTagName("html")[0].classList.add("dark");
 })
@@ -40,101 +45,98 @@ onUnmounted(() => {
 
 <style scoped lang="less">
 .common-layout {
-    height: 100%;
     width: 100%;
-    user-select: none;
-    .div-f {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, 360px);
-        grid-auto-flow: row;
-        grid-auto-columns: minmax(0, 1fr);
-        grid-gap: 20px;
-        justify-content: center;
+    height: 100%;
+    overflow: auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 20px;
+    padding: 0 10px;
+    overflow: auto;
+    box-sizing: border-box;
 
-        .el-card {
-            .div-head {
-                margin-top: -40px;
+    .el-card {
+        width: 100%;
+        height: 475px;
+        border-radius: 20px;
+        .div-head {
+            margin-top: -35px;
+            height: 30px;
+            line-height: 30px;
+            text-align: center;
+        }
+        .inner {
+            height: 420px;
+            width: inherit;
+            overflow: hidden;
+
+            .hot-list-article li {
+                display: block;
+                line-height: 32px;
+                position: relative;
+                counter-increment: nums;
+                padding-left: 80px;
+                width: inherit;
+                margin-left: -40px;
+            }
+            .hot-list-article li a {
+                position: relative;
+                left: -40px;
+                color: #787977;
+                text-decoration: none;
+            }
+
+            .hot-list-article li:before {
+                color: #000;
+                width: 22px;
+                height: 22px;
+                line-height: 22px;
                 text-align: center;
+                content: counter(nums, decimal);
+                position: absolute;
+                left: 0;
+                top: 5px;
+                border-radius: 100%;
+                background-color: #edefee;
+                text-shadow: 0 1px 0 rgba(255, 255, 255, .5);
+                font-family: SourceCodeProRegular, Menlo, Monaco, Consolas, "Courier New", monospace, 'Helvetica Neue', Arial, sans-serif
             }
 
-            width: 360px;
-            border-radius: 20px;
-            
-            .inner {
-                height: 420px;
-                overflow: hidden;
-                margin: -20px 0 0 -40px;
-
-                .hot-list-article li {
-                    display: block;
-                    line-height: 32px;
-                    position: relative;
-                    counter-increment: nums;
-                    padding-left: 80px;
-                    max-width: 260px;
-                }
-                .hot-list-article li a:hover {
-                    cursor: url("./assets/mouse.png"), default !important;;
-                }
-                .hot-list-article li a {
-                    position: relative;
-                    left: -40px;
-                    color: #787977;
-                    text-decoration: none;
-                }
-
-                .hot-list-article li:before {
-                    color: #000;
-                    width: 22px;
-                    height: 22px;
-                    line-height: 22px;
-                    text-align: center;
-                    content: counter(nums, decimal);
-                    position: absolute;
-                    left: 0;
-                    top: 5px;
-                    border-radius: 100%;
-                    background-color: #edefee;
-                    text-shadow: 0 1px 0 rgba(255, 255, 255, .5);
-                    font-family: SourceCodeProRegular, Menlo, Monaco, Consolas, "Courier New", monospace, 'Helvetica Neue', Arial, sans-serif
-                }
-
-                .hot-list-article li:first-child:before,
-                .hot-list-article li:nth-child(2):before,
-                .hot-list-article li:nth-child(3):before {
-                    color: #fff;
-                    text-shadow: none
-                }
-
-                .hot-list-article li:first-child:before {
-                    background-color: #e24d46
-                }
-
-                /*第1行的行号样式*/
-                .hot-list-article li:nth-child(2):before {
-                    background-color: #2ea7e0
-                }
-
-                /*第2行的行号样式*/
-                .hot-list-article li:nth-child(3):before {
-                    background-color: #6bc30d
-                }
-
-                /*第3行的行号样式*/
-                .hot-list-article li:hover {
-                    a {
-                        text-decoration: underline;
-                        font-size: large;
-                        display: inline-block;
-                        transition: transform 0.3s ease-in-out;
-                        transform: translateX(5px);
-                    }
-                }
+            .hot-list-article li:first-child:before,
+            .hot-list-article li:nth-child(2):before,
+            .hot-list-article li:nth-child(3):before {
+                color: #fff;
+                text-shadow: none
             }
 
-            .inner:hover {
-                overflow: auto;
+            .hot-list-article li:first-child:before {
+                background-color: #e24d46
             }
+
+            /*第1行的行号样式*/
+            .hot-list-article li:nth-child(2):before {
+                background-color: #2ea7e0
+            }
+
+            /*第2行的行号样式*/
+            .hot-list-article li:nth-child(3):before {
+                background-color: #6bc30d
+            }
+
+            /*第3行的行号样式*/
+            .hot-list-article li:hover {
+                a {
+                    text-decoration: underline;
+                    font-size: large;
+                    display: inline-block;
+                    transition: transform 0.3s ease-in-out;
+                    transform: translateX(5px);
+                }
+            }
+        }
+
+        .inner:hover {
+            overflow: auto;
         }
     }
 }
